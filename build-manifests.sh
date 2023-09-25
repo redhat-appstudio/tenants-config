9fe067be236eb2d63246c36fbca2040eca4bdea9
Ernesto González <ergonzal@redhat.com>
2023-09-25 15:49:02 +0200

    Add compatibility with .yml and cleaner output
    - Add compatibility with kustomization.yml instead of kustomization.yaml
    - Clean the output of the build manifests to make it more user-friendly
    
    Signed-off-by: Ernesto González <ergonzal@redhat.com>

--- build-manifests.sh
+++ build-manifests.sh
@@ -1,14 +1,15 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 main() {
     local dirs
-    dirs=($(find . -name kustomization.y?ml | xargs dirname))
+    dirs=($(find . \( -name 'kustomization.yaml' -o -name 'kustomization.yml' \) | xargs dirname))
     local out=auto-generated
     local ret=0
     rm -rf "$out"
     mkdir -p "$out"
 
     for d in "${dirs[@]}"; do
+        echo "Checking directory ${d}"
         o="${out}/${d}"
         mkdir -p "$o"
         if ! kustomize build -o "$o" "$d"; then
